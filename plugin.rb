0bc14757880f3ad0f82ed4164c22b3ccf21fcd0f
misaka4e21 <misaka4e21@gmail.com>
2024-03-31 18:32:07 +0800

    delete without callbacks

--- plugin.rb
+++ plugin.rb
@@ -7,7 +7,7 @@
 after_initialize do
   plugin = Plugin::Instance.new
   cb = Proc.new do |user, guardian, opts|
-    Post.where(user_id: user.id).destroy_all
+    Post.where(user_id: user.id).delete_all
   end
   plugin.register_user_destroyer_on_content_deletion_callback(cb)
 end
